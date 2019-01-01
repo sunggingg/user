37eec5338d1acbe566953de6cf7c8c6ec97e9cad
sunggingg <46112680+sunggingg@users.noreply.github.com>
2019-01-02 02:34:10 +0700

    Update user.php

--- user.php
+++ user.php
@@ -9,5 +9,5 @@ $figlet= $ijo."
 ── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
 ── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
   $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
-  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." 73jqem    ".$t;
+  $putih."channel :".$ijo." adi bordir ".$putih."[--] channel:".$ijo." sungging  ".$t.
+  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." otr3o8    ".$t;
